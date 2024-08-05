0415956549de954f894e849f5da6466aba063b8d
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-08-05 16:24:03 +0200

    [IMP] add sorting by category of addons for local modules

--- src/odoo_sort_manifest_depends/sort_manifest_deps.py
+++ src/odoo_sort_manifest_depends/sort_manifest_deps.py
@@ -15,6 +15,8 @@ from manifestoo_core.odoo_series import OdooSeries
 
 MANIFEST_NAMES = ("__manifest__.py", "__openerp__.py", "__terp__.py")
 
+NAME_DEFAULT_CATEGORY = "Default"
+
 
 def _generate_depends_sections(dict_depends_by_cateogry):
     new_content = '"depends": ['
@@ -26,7 +28,19 @@ def _generate_depends_sections(dict_depends_by_cateogry):
     return new_content
 
 
-def do_sorting(addons_dir, odoo_version):
+def _generate_dict_addons_obj(addons_dir):
+    addons = listdir(addons_dir)
+    local_addons = {}
+    for addon_name in addons:
+        addon_dir = Path(join(addons_dir, addon_name))
+        if not is_addon_dir(addon_dir, allow_not_installable=False):
+            continue
+        addon_obj = Addon.from_addon_dir(addon_dir, allow_not_installable=False)
+        local_addons[addon_name] = addon_obj
+    return local_addons
+
+
+def do_sorting(addons_dir, odoo_version, project_name):
     """
     Update manifest files to sort dependencies by typa and then by name.
 
@@ -37,20 +51,20 @@ def do_sorting(addons_dir, odoo_version):
     The script will also exclude not installable addons from the dependencies.
     """
     odoo_version = OdooSeries(odoo_version)
-    addons = listdir(addons_dir)
-    for addon_name in addons:
-        addon_dir = Path(join(addons_dir, addon_name))
 
-        if not is_addon_dir(addon_dir, allow_not_installable=False):
-            continue
+    local_addons = _generate_dict_addons_obj(addons_dir)
 
-        addon_obj = Addon.from_addon_dir(addon_dir, allow_not_installable=False)
+    for addon_obj in local_addons.values():
         dependencies = addon_obj.manifest.depends
 
-        odoo_ce, odoo_ee, other, custom = [], [], [], []
+        odoo_ce, odoo_ee, other = [], [], []
+        custom_by_category = {}
         for dep in dependencies:
-            if dep in addons:
-                custom.append(dep)
+            if local_addons.get(dep):
+                addons_in_category = custom_by_category.setdefault(
+                    local_addons.get(dep).manifest.manifest_dict.get("category", NAME_DEFAULT_CATEGORY), []
+                )
+                addons_in_category.append(dep)
             elif is_core_ce_addon(dep, odoo_version):
                 odoo_ce.append(dep)
             elif is_core_ee_addon(dep, odoo_version):
@@ -58,21 +72,29 @@ def do_sorting(addons_dir, odoo_version):
             else:
                 other.append(dep)
 
-        if not (custom or odoo_ce or odoo_ee or other):
+        if not (custom_by_category or odoo_ce or odoo_ee or other):
             continue
 
-        custom, odoo_ce, odoo_ee, other = sorted(custom), sorted(odoo_ce), sorted(odoo_ee), sorted(other)
+        odoo_ce, odoo_ee, other = sorted(odoo_ce), sorted(odoo_ee), sorted(other)
 
         manifest_path = addon_obj.manifest_path
         with open(manifest_path) as f:
             content = f.read()
 
+        local_categories = {}
+        for cat, addon_names in custom_by_category.items():
+            if cat == NAME_DEFAULT_CATEGORY:
+                local_categories[project_name] = sorted(addon_names)
+            else:
+                local_categories[f"{project_name}/{cat}"] = sorted(addon_names)
+
         categories = {
             "Odoo Community": odoo_ce,
             "Odoo Enterprise": odoo_ee,
             "Others (OCA,Shopinvader,...)": other,
-            "Local": custom,
         }
+
+        categories.update(local_categories)
 
         new_content = _generate_depends_sections(categories)
 
@@ -95,5 +117,11 @@ def do_sorting(addons_dir, odoo_version):
     required=True,
     help="Project's Odoo version (e.g. 16.0)",
 )
-def sort_manifest_deps(local_addons_dir, odoo_version):
-    do_sorting(local_addons_dir, odoo_version)
+@option(
+    "--project-name",
+    type=str,
+    help="Name of the project, will be the name of category of local addons",
+    default="Local",
+)
+def sort_manifest_deps(local_addons_dir, odoo_version, project_name):
+    do_sorting(local_addons_dir, odoo_version, project_name)
